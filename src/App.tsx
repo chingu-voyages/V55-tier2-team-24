f0ca8bd593364895534c28582b130baaa05ab7b3
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-05-22 13:36:09 -0400

    Footer imported into app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <main className="h-screen w-full bg-slate-950">
       <Header/>
-      
+      <section></section>
+      <Footer/>
     </main>
   );
 }
