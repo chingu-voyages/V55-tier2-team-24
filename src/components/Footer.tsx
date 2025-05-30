67aacce358cdae03366a1dff8b98a1dd6f03224e
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-05-30 18:26:22 -0500

    Refactor Footer component to match Figma design and layout, and make contributor display dynamic

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,80 +1,81 @@
 export default function Footer() {
+  const contributors = [
+    {
+      avatar:
+        "https://media.licdn.com/dms/image/v2/C4E03AQHJ5b6j96iNug/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1611860936962?e=1753920000&v=beta&t=FHPGdOiOcctc1wMlSv6t6tMnXAgSAGQdSMAm--P3LZY",
+      name: "Viral Barot",
+      role: "Product Owner",
+    },
+    {
+      avatar:
+        "https://media.licdn.com/dms/image/v2/D5635AQE5aJZSGcp1rA/profile-framedphoto-shrink_800_800/B56ZV5YWcEGsAo-/0/1741498181741?e=1749254400&v=beta&t=mZcBL8iolShHVdAgeikHEmE5GMuw7_1CYSkMnYjckDg",
+      name: "Jennie Glass",
+      role: "UI/UX",
+    },
+    {
+      avatar:
+        "https://media.licdn.com/dms/image/v2/C4E03AQHMeCNJ4TEzpw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1645063589733?e=1753920000&v=beta&t=e2RyAysTXUEjBzMHlLVbGLUpVN4sdT9uc_JMK9kQxIY",
+      name: "Nathan Walker",
+      role: "Scrum Master",
+    },
+    {
+      avatar:
+        "https://media.licdn.com/dms/image/v2/D4D35AQEi-Rs75YpsCA/profile-framedphoto-shrink_800_800/profile-framedphoto-shrink_800_800/0/1690907819752?e=1749254400&v=beta&t=UoarTrzht2Qskahe35q4eYxIcO47i97jm3E27Jlfpz8",
+      name: "Maria Nathalie Chejin",
+      role: "Shadow Scrum Master",
+    },
+    {
+      avatar: "https://avatars.githubusercontent.com/u/106914406",
+      name: "Ivan Rebolledo",
+      role: "Developer",
+    },
+    {
+      avatar: "https://avatars.githubusercontent.com/u/158505324",
+      name: "Stephanie Leon",
+      role: "Developer",
+    },
+    {
+      avatar: "https://avatars.githubusercontent.com/u/128321147",
+      name: "Christin Martin",
+      role: "Developer",
+    },
+    {
+      avatar: "https://avatars.githubusercontent.com/u/170786939",
+      name: "Ramon Zambrano",
+      role: "Developer",
+    },
+  ];
+
   return (
-    <footer className=" flex items-center text-white text-sm px-2 py-3 w-full h-20 border-t-1 border-gray-400">
-      <section className="w-[70%] m-3">
+    <footer className="flex items-center justify-between text-sm px-4 py-3 w-full h-20 border-1 border-[#E5E7EB] bg-[#F9FAFB]">
+      <section>
         <a
-          className="flex  gap-x-2 hover:underline underline-offset-2"
+          className="flex items-center gap-2 hover:underline underline-offset-2"
           href="https://github.com/chingu-voyages/V55-tier2-team-24"
         >
           <img
             src="/images/githubIcon.svg"
             alt="Github icon"
-            width={20}
-            height={20}
+            width={30}
+            height={30}
             className="bg-gray-100 rounded p-1"
-          />
-          GitHub Repo
+          ></img>
+          <span>GitHub Repo</span>
         </a>
       </section>
-      <section className="w-[30%] flex justify-end items-center">
+      <section className="flex items-center">
         <p>Contributors:</p>
         <div className="flex">
-          <img
-            src="https://placehold.co/200"
-            alt="Placeholder contributor icons"
-            width={20}
-            height={20}
-            className="h-full rounded-full m-1"
-          />
-          <img
-            src="https://placehold.co/200"
-            alt="Placeholder contributor icons"
-            width={20}
-            height={20}
-            className="h-full rounded-full m-1"
-          />
-          <img
-            src="https://placehold.co/200"
-            alt="Placeholder contributor icons"
-            width={20}
-            height={20}
-            className="h-full rounded-full m-1"
-          />
-          <img
-            src="https://placehold.co/200"
-            alt="Placeholder contributor icons"
-            width={20}
-            height={20}
-            className="h-full rounded-full m-1"
-          />
-          <img
-            src="https://placehold.co/200"
-            alt="Placeholder contributor icons"
-            width={20}
-            height={20}
-            className="h-full rounded-full m-1"
-          />
-          <img
-            src="https://placehold.co/200"
-            alt="Placeholder contributor icons"
-            width={20}
-            height={20}
-            className="h-full rounded-full m-1"
-          />
-          <img
-            src="https://placehold.co/200"
-            alt="Placeholder contributor icons"
-            width={20}
-            height={20}
-            className="h-full rounded-full m-1"
-          />
-          <img
-            src="https://placehold.co/200"
-            alt="Placeholder contributor icons"
-            width={20}
-            height={20}
-            className="h-full rounded-full m-1"
-          />
+          {contributors.map((contributor, index) => (
+            <img
+              key={index}
+              src={contributor.avatar}
+              alt={contributor.name}
+              width={30}
+              height={30}
+              className="h-full rounded-full m-1"
+            />
+          ))}
         </div>
       </section>
     </footer>
