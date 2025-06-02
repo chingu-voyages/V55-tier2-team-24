bc05a36aa5f584c8ec4673fbcbed785cf1293865
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-06-02 16:46:12 -0400

    Chatbot added to Discovery page,

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -5,22 +5,31 @@ import ResourcesC from "../components/ResourcesC";
 import ResourceType from "../components/ResourceType";
 import TagsContainer from "../components/TagsContainer";
 // import Topics from "../components/Topics";
+import ChatBotContainer from "../components/ChatbotContainer";
 
 export default function Discover() {
   return (
-    <main className="flex min-h-screen bg-white w-full">
-      <aside className="w-96 p-4 border-gray-400">
-        <Form />
-        <TagsContainer />
-        <Authors />
-        {/* replace resource type with topics */}
-        {/* <Topics /> */}
-        <ResourceType />
-        <ResetFilters />
-      </aside>
-      <section className="flex flex-col items-start overflow-auto flex-1 p-6">
-        <ResourcesC />
-      </section>
+    <main className="flex flex-col min-h-screen bg-white w-full">
+      <div className="flex flex-1 w-full">
+        <aside className="w-96 p-4 border-gray-400">
+          <Form />
+          <TagsContainer />
+          <Authors />
+          {/* replace resource type with topics */}
+          {/* <Topics /> */}
+          <ResourceType />
+          <ResetFilters />
+        </aside>
+  
+        <section className="flex flex-col items-start overflow-auto flex-1 p-6">
+          <ResourcesC />
+        </section>
+      </div>
+  
+      {/* Chatbot container aligned to bottom right of the full layout */}
+      <div className="w-full flex justify-end px-6 pb-6">
+        <ChatBotContainer />
+      </div>
     </main>
   );
 }
