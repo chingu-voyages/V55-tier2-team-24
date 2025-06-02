9d158f494107de999ea36aaa0d27c31e704a0556
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-06-02 16:48:32 -0400

    Suggested Questions

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -2,7 +2,16 @@ import { useState, type FormEvent } from 'react'
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export default function Prompt() {
-    const [aiAnswer, setAiAnswer] = useState<string>(''); // Declare a state variable...
+    // Suggested questions given to users
+    const suggestions = [
+        "What tags can I search for?",
+        "What types of resources can I search for?",
+        "What authros have contributed to this database?",
+        "How do I save or remove a favorite resource from my list?",
+        "Where can I view my saved resources?",
+        "How do I view all the results of a search?",
+    ];
+
 
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         // Prevent the browser from reloading the page
