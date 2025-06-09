0b9d95299b8dc5389d58a8dc4f6bc1892081f65e
ivannissimrch <ivannissimrch@gmail.com>
2025-06-09 11:25:23 -0400

    Update place holders

--- src/helpers/placeHolders.ts
+++ src/helpers/placeHolders.ts
@@ -1,15 +1,8 @@
 export const searchPlaceHolders = [
-  "Ready to search! 😊",
-  "Type a keyword, like Javascript",
-  "Type a skill, like React",
-  "What do you want to learn today?",
-  "Search for a topic, like CSS",
-  "Looking for something specific?",
+  "Search for how to build a portfolio",
+  "What is a Chingu voyage",
+  "Search for how to use Git on a team",
+  "Try how to become a 10x developer",
+  "Search for frontend interview tips",
+  "How to get hired without a cs degree",
 ];
-
-///What to expect from Chingu
-///How to get hired without a CS degree
-///How to use Git in a team
-///Working with designers
-///AI engineer roadmap
-///Why daily standups matter
