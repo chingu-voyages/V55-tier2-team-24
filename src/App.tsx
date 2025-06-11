2ee7c170fdeb2f54c2c87c855990762225a23773
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-06-11 10:49:36 -0400

    Adding About Page back to be changed to Favorites

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./pages/Root.tsx";
 import Home from "./pages/Home.tsx";
 import StoreContextProvider from "./context/StoreContext.js";
+import About from "./pages/About.tsx";
 import Discover from "./pages/Discover.tsx";
 import BrokenURLPage from "./components/BrokenURLPage.tsx"
 
@@ -19,6 +20,10 @@ export default function App() {
         {
           path: "Discover",
           element: <Discover />,
+        },
+        {
+          path: "About",
+          element: <About />,
         },
         {
           path: "*",
