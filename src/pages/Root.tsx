e5df4a7721e3239c53b623a66043673d4a070da1
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-06-11 10:36:07 -0400

    Spacing around the Chatbot button

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -22,7 +22,7 @@ export default function RootLayout() {
       </main>
 
       {/* Chatbot container aligned to bottom right of the full layout */}
-      <div className="w-full flex justify-end px-6 pb-6">
+      <div className="w-full flex justify-end p-6">
         <ChatBotContainer />
       </div>
       <Footer />
